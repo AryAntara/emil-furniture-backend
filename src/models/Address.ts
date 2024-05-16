dee716e88c04874e1c1118af4d27a49c5093caf5
ary <ikomangaryantara382@gmail.com>
2024-05-16 21:24:52 +0800

    feat (address): initiate files (models, migrations, controller, and module)

---
+++ src/models/Address.ts
@@ -0,0 +1,26 @@
+import { Model, DataTypes } from 'sequelize';
+import { sequelize } from "../database"
+import { User } from './User';
+
+export class Address extends Model {}
+
+Address.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+          },
+          userId: DataTypes.INTEGER,
+          title: DataTypes.STRING,
+          description: DataTypes.STRING,
+          isActivated: DataTypes.ENUM('yes', 'no'),
+          createdAt: DataTypes.DATE,
+          updatedAt: DataTypes.DATE,
+          deletedAt: DataTypes.DATE
+    },
+    { sequelize, modelName: 'address', paranoid: true },
+);
+
+Address.hasOne(User)
+
